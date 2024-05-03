7b2b566700799d3ea67d49f7aa4e800bcfe6adc4
munenakoppa <30587898+munenakoppa@users.noreply.github.com>
2024-05-03 17:56:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MobileList from './Components/MobileList';
+
+function App() {
+  return (
+    <div className="App">
+      <MobileList />
+    </div>
+  );
+}
+
+export default App;
